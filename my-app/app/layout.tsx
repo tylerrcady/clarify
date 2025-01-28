3d2c0d54326c56b6baea7d1d1a3182c228614d44
Tyler Cady <tylercadyfairport@gmail.com>
2025-01-27 23:55:44 -0500

    minor changes

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
                                 {children}
                             </div>
 
-                            <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-4 py-4">
+                            <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-2 py-4">
                                 <p>
                                     An application by{" "}
                                     <a
