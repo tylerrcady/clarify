6c52795d6796ec24d15aceec7def7ecb490c28fb
Tyler Cady <tylercadyfairport@gmail.com>
2025-03-02 23:44:56 -0500

    cookie warning fixes

--- my-app/utils/supabase/middleware.ts
+++ my-app/utils/supabase/middleware.ts
@@ -21,15 +21,19 @@ export const updateSession = async (request: NextRequest) => {
                         return request.cookies.getAll();
                     },
                     setAll(cookiesToSet) {
-                        cookiesToSet.forEach(({ name, value }) =>
-                            request.cookies.set(name, value)
-                        );
                         response = NextResponse.next({
-                            request,
+                            request: {
+                                headers: request.headers,
+                            },
                         });
-                        cookiesToSet.forEach(({ name, value, options }) =>
-                            response.cookies.set(name, value, options)
-                        );
+
+                        cookiesToSet.forEach(({ name, value, options }) => {
+                            response.cookies.set({
+                                name,
+                                value,
+                                ...options,
+                            });
+                        });
                     },
                 },
             }
