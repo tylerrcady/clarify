bba07b58698dc22d4f51fea9db503d6b068baa5c
Tyler Cady <tylercadyfairport@gmail.com>
2025-03-16 13:22:05 -0400

    note to self

--- my-app/app/api/threads/[threadId]/comments/route.ts
+++ my-app/app/api/threads/[threadId]/comments/route.ts
@@ -36,6 +36,7 @@ export async function GET(request: NextRequest) {
 }
 
 function getRandomName(usedNames: string[]): string {
+    // be careful regarding scalability
     const adjectives = [
         "Quick",
         "Lazy",
